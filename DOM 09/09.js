2c5fbd5be55a2db5e07966bfb86478c9011b0bd2
PabloCsDev <devpablocarvalho@gmail.com>
2023-05-27 15:16:21 -0300

    update

---
+++ DOM 09/09.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var p1 = window.document.getElementsByTagName("p")[1];
+    var texto = document.createTextNode("Está escrito assim: " + p1.innerText);
+    var div = document.createElement("div");
+    div.appendChild(texto);
+    document.body.appendChild(div);
+  });
+  
+  window.addEventListener('DOMContentLoaded', function() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white"; // Define a cor do texto como branco
+  });
+  
+  window.addEventListener('DOMContentLoaded', function() {
+    var p1 = window.document.getElementsByTagName("p")[1];
+    p1.style.fontFamily = "monospace";
+    p1.style.fontStyle = "italic";
+  });
+  

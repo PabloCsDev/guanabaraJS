18f0d4a4f14999ff419b369ea6467a208d6b2160
devpablocarvalho@gmail.com <devpablocarvalho@gmail.com>
2023-06-20 23:45:47 -0300

    update

---
+++ 10/exercicio 01/10.js
@@ -0,0 +1,18 @@
+function clicar() {
+    var a = window.document.getElementById("area");
+    a.textContent = "Clicou!";
+    a.style.background = 'red'
+  }
+  
+  function entrar() {
+    var a = window.document.getElementById("area");
+    a.textContent = "Entrou!";
+    a.style.background = 'green'
+  }
+  
+  function sair() {
+    var a = window.document.getElementById("area");
+    a.textContent = "Saiu!";
+    a.style.background = 'red'
+  }
+  
---
+++ 10/exercicio 02/10.js
@@ -0,0 +1,11 @@
+
+function somar() {
+    var tn1 = window.document.getElementById('txtn1');
+    var tn2 = window.document.querySelector('input#txtn2');
+    var res = window.document.getElementById('res');
+    var n1 = Number(tn1.value);
+    var n2 = Number(tn2.value);
+    var s = n1 + n2;
+    res.innerHTML = `A SOMA ENTRE ${n1} E ${n2} É IGUAL A ${s}`;
+  }
+  
